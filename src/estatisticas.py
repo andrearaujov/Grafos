0e7e8fcccc3c36b011f4d1e552723c54a0c3372a
ANDRÉ ARAÚJO MENDONÇA <andremendoncamineiro@gmail.com>
2025-04-11 02:09:41 -0300

    pre processamento de dados

--- src/estatisticas.py
+++ src/estatisticas.py
@@ -1,3 +1,5 @@
+# src/estatisticas.py
+
 from grafo import Grafo
 
 def carregar_grafo(caminho_arquivo):
@@ -10,9 +12,9 @@ def calcular_estatisticas(grafo):
         "Quantidade de vértices": grafo.quantidade_vertices(),
         "Quantidade de arestas": grafo.quantidade_arestas(),
         "Quantidade de arcos": grafo.quantidade_arcos(),
-        "Quantidade de vértices requeridos": len(grafo.vertices_requeridos),
-        "Quantidade de arestas requeridas": len(grafo.arestas_requeridas),
-        "Quantidade de arcos requeridos": len(grafo.arcos_requeridos),
+        "Quantidade de vértices requeridos": grafo.quantidade_vertices_requeridos(),
+        "Quantidade de arestas requeridas": grafo.quantidade_arestas_requeridas(),
+        "Quantidade de arcos requeridos": grafo.quantidade_arcos_requeridos(),
         "Densidade do grafo": grafo.densidade(),
         "Componentes conectados": grafo.componentes_conectados(),
         "Grau mínimo": grafo.grau_minimo(),
@@ -22,4 +24,3 @@ def calcular_estatisticas(grafo):
         "Diâmetro": grafo.diametro()
     }
     return estatisticas
-
